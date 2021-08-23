23b2aaad4169176b94beecd30b7a460950be3e1d
Debbie <debbiecamera0704@gmail.com>
2021-08-24 02:46:26 +0800

    feat: add login feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const userService = require('../services/userService')
 const validation = require('../utils/validate')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userController = {
   register: async (req, res, next) => {
@@ -12,6 +13,46 @@ const userController = {
 
       await userService.register({ account, password: hash, email, name, phone })
       return res.json({ status: 'success', message: '成功註冊' })
+    }
+    catch (err) {
+      next(err)
+    }
+  },
+
+  login: async (req, res, next) => {
+    try {
+      const { account, password } = req.body
+
+      if (!account || !password) {
+        throw new Error('All fields are required.')
+      }
+
+      const user = await userService.login(account)
+
+      if (!user) {
+        throw new Error('The account does not exist.')
+      }
+      if (!bcrypt.compareSync(password, user.password)) {
+        throw new Error('incorrect password')
+      }
+
+      const payload = { id: user.id }
+      const token = jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        { expiresIn: 43200 }
+      )
+
+      return res.json({
+        status: 'success',
+        token,
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          avatar: user.avatar
+        }
+      })
     }
     catch (err) {
       next(err)
--- services/userService.js
+++ services/userService.js
@@ -3,6 +3,13 @@ const { User } = require('../models')
 const userService = {
   register: (data) => {
     User.create(data)
+  },
+
+  login: (account) => {
+    return User.findOne({
+      where: { account },
+      attributes: ['id', 'password', 'name', 'avatar', 'email']
+    })
   }
 }
 
