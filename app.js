c219caed71245066ba57a8d66a37f47c1d722c5b
debbie8820 <debbiecamera0704@gmail.com>
2021-09-19 15:35:37 +0800

    install cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const { urlencoded } = require('express')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 if (process.env.NODE_ENV !== "production") {
   require('dotenv').config()
 }
 
+app.use(cors())
 app.use(urlencoded({ extended: true }))
 app.use(express.json())
 
